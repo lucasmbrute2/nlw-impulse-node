ad55048460227a3f8f504d53f056d91a052f3860
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-05-09 20:35:01 -0300

    feature: add mail trap integration

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
 
 const app = express();
 app.use(express.json());
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "59f34adf789a5a",
+        pass: "a5829b09cf04d5",
+    },
+});
 
 app.post("/feedbacks", async (req, res) => {
     const { type, comment, screenshot } = req.body;
@@ -13,6 +23,18 @@ app.post("/feedbacks", async (req, res) => {
             comment,
             screenshot,
         },
+    });
+
+    await transport.sendMail({
+        from: "Equipe Feedget <oi@feedget.com>",
+        to: "Lucas Souza <lucasmbrute614@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback ${type}</p>`,
+            `<p>Comentário ${comment}</p>`,
+            `</div>`
+        ].join("\n"),
     });
 
     return res.status(201).json({ data: feedback });
