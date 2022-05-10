31d91c31a2d6d2a7a721d714dd47c942635900e1
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-05-10 11:42:15 -0300

    feat: add full test

--- src/use-cases/submit-feedback-use-cases.spec.ts
+++ src/use-cases/submit-feedback-use-cases.spec.ts
@@ -1,16 +1,46 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 
-describe("Submit feedback", () => {
-    it("should able to submit a feedback", async() => {
-        const submitFeedback = new SubmitFeedbackUseCase(
-            { create: async () => {} },
-            { sendEmail: async () => {} }
-        );
+const submitFeedback = new SubmitFeedbackUseCase(
+    { create: async () => {} },
+    { sendEmail: async () => {} }
+);
 
-        await expect(submitFeedback.execute({
-            type: "BUG",
-            comment: "example comme nt",
-            screenshot: "test.jpg"
-        })).resolves.not.toThrow()
+describe("Submit feedback", () => {
+    it("should able to submit a feedback", async () => {
+        await expect(
+            submitFeedback.execute({
+                type: "BUG",
+                comment: "example comme nt",
+                screenshot: "data:image/png;base64/test.jpg",
+            })
+        ).resolves.not.toThrow();
+    });
+
+    it("should not be able to submit feedback without type", async () => {
+        await expect(
+            submitFeedback.execute({
+                type: "",
+                comment: "example content",
+            })
+        ).rejects.toThrow();
+    });
+
+    it("should not be able to submit feedback without comment", async () => {
+        await expect(
+            submitFeedback.execute({
+                type: "BUG",
+                comment: "",
+            })
+        ).rejects.toThrow();
+    });
+
+    it("should not be able to submit feedback with an invalid screenshot", async () => {
+        await expect(
+            submitFeedback.execute({
+                type: "BUG",
+                comment: "Bugou tudo",
+                screenshot: "test.jpg",
+            })
+        ).rejects.toThrow();
     });
 });
