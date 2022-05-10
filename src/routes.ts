486312e8fb7a0d7c4beb7e7cca82c5a39a18438f
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-05-09 23:38:59 -0300

    refactoring: creating route file to POST feedback

---
+++ src/routes.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+export const routes = express.Router();
+
+const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+        user: "59f34adf789a5a",
+        pass: "a5829b09cf04d5",
+    },
+});
+
+
+routes.post("/feedbacks", async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        },
+    });
+
+    await transport.sendMail({
+        from: "Equipe Feedget <oi@feedget.com>",
+        to: "Lucas Souza <lucasmbrute614@gmail.com>",
+        subject: "Novo feedback",
+        html: [
+            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
+            `<p>Tipo do feedback ${type}</p>`,
+            `<p>Comentário ${comment}</p>`,
+            `</div>`,
+        ].join("\n"),
+    });
+
+    return res.status(201).json({ data: feedback });
+});
--- src/server.ts
+++ src/server.ts
@@ -1,44 +1,13 @@
 import express from "express";
-import { prisma } from "./prisma";
-import nodemailer from "nodemailer";
+import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
+app.use(routes)
 
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: "59f34adf789a5a",
-        pass: "a5829b09cf04d5",
-    },
-});
 
-app.post("/feedbacks", async (req, res) => {
-    const { type, comment, screenshot } = req.body;
 
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        },
-    });
 
-    await transport.sendMail({
-        from: "Equipe Feedget <oi@feedget.com>",
-        to: "Lucas Souza <lucasmbrute614@gmail.com>",
-        subject: "Novo feedback",
-        html: [
-            `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
-            `<p>Tipo do feedback ${type}</p>`,
-            `<p>Comentário ${comment}</p>`,
-            `</div>`
-        ].join("\n"),
-    });
-
-    return res.status(201).json({ data: feedback });
-});
 
 app.listen(3333, () => {
     console.log("Server is running!");
