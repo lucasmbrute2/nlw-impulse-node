d63a0ce82744317fed16f2da738da77ad8fa12df
lucasmbrute2 <68877260+lucasmbrute2@users.noreply.github.com>
2022-05-09 23:53:17 -0300

    feature: prisma repository (abstracting)

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,7 @@ import { routes } from "./routes";
 
 const app = express();
 app.use(express.json());
-app.use(routes)
-
-
-
-
+app.use(routes);
 
 app.listen(3333, () => {
     console.log("Server is running!");
